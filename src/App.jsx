45d95a307a118d3e2d2272f6c4685a04a805dcc8
hase <s2110327@coins.tsukuba.ac.jp>
2023-10-25 15:00:33 +0900

    話題例の表示

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
       <h1>e-talk support</h1>
-    </>
+      <ul>
+        <li>新潟の伝統的なお土産「笹団子」</li>
+        <li>越後湯沢のスキー場</li>
+        <li>日本三大花火の「長岡花火」</li>
+      </ul>
+    </div>
   );
 }
 
