b012b0424e0a2858d6693b8231cc2c41c9823a5b
pictomo <tomo.pict@gmail.com>
2023-12-01 17:04:17 +0900

    [update]meetingを2つ追加した

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,8 @@ function App() {
   return (
     <div>
       <h1>e-talk support</h1>
+      <Meeting meetingId="BLcYePnoqOCCluUFcwQZ"></Meeting>
+      <Meeting meetingId="h33Kj2ntEkv7QnopygGY"></Meeting>
       <Meeting meetingId="CuqhDPfaaApE15piGpP2"></Meeting>
     </div>
   );
