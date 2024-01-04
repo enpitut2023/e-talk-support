4ad5cd25bc5606f86308905ccd71ff48b45144a8
pictomo <101302703+pictomo@users.noreply.github.com>
2024-01-05 00:36:27 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
